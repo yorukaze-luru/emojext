cbff2a6445fbd3e930e7d518a83fdeef15115263
yorukaze-luru <46132263+yorukaze-luru@users.noreply.github.com>
2023-07-09 10:47:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='emojext',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+    ],
+)
