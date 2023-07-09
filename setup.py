f044afea672cb6b30c834d6ef89ad9f880f5e98c
yorukaze-luru <46132263+yorukaze-luru@users.noreply.github.com>
2023-07-09 10:53:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='emojext',
     version='0.1.0',
+    url="https://github.com/yorukaze-luru/emojext",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'discord.py',
     ],
